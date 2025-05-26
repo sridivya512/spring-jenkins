229cc602e64e0b83ac9cbcae4a55f6267b9f8d0b
sridivyadurshetty <dsridivya5@gmail.com>
2025-05-26 18:33:07 -0500

    second commit

--- src/test/java/com/divya/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/divya/jenkins/api/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing 2nd log statement");
 		assertEquals(true, true);
 	}
 
